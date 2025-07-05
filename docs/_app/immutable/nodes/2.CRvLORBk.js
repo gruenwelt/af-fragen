d68969992393a7b3087ee68a4678955626ac4524
gruenwelt <gruenwelt@users.noreply.github.com>
2025-07-05 15:35:28 +0000

    added mobile view to main page

---
+++ docs/_app/immutable/nodes/2.CRvLORBk.js
@@ -0,0 +1,1 @@
+import"../chunks/CWj6FrbW.js";import{F as n,G as a,I as p}from"../chunks/DcTbUsQr.js";import{s}from"../chunks/CMUKJEYv.js";const i=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=a(e);s(t,()=>r.children),p(o,e)}export{d as component,u as universal};
